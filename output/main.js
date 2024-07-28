e282d94984a6f2f5719a52729a0dfcaf2479bd88
doedash <146763271+doedash@users.noreply.github.com>
2024-07-28 02:21:36 +0000

    setup ts

---
+++ output/main.js
@@ -0,0 +1,6 @@
+var input = [
+    'abc',
+    'xbz'
+];
+function solution(input) {}
+console.log(solution(input) === 'axbcz');
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+const input: string[] = [
+    'abc',
+    'xbz'
+]
+
+
+function solution(input: string[]): string {
+
+}
+
+console.log(solution(input) === 'axbcz');
